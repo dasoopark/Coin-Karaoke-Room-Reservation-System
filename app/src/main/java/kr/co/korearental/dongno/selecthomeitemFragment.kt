05775b972a2f6c774d3afd7f67c7f2e0dec70dd0
choi3179 <66417882+choi3179@users.noreply.github.com>
2020-08-07 01:35:14 +0900

    08/06
    리뷰 xml 수정
    
    리뷰 리사이클러뷰 출력 및 각 DB에 저장 가능
    
    즐겨찾기 별 true false에 따른 DB 저장 및 삭제 가능
    
    리뷰 개수와 즐겨찾기 별 색깔 변환 문제

--- app/src/main/java/kr/co/korearental/dongno/selecthomeitemFragment.kt
+++ app/src/main/java/kr/co/korearental/dongno/selecthomeitemFragment.kt
@@ -3,10 +3,12 @@ package kr.co.korearental.dongno
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -23,17 +25,17 @@ import kotlinx.android.synthetic.main.selecthomeitemfragment.*
 import kotlinx.android.synthetic.main.selecthomeitemfragment.view.*
 
 class selecthomeitemFragment : Fragment(){
-    var check  = false
-    var listreview = arrayListOf<infoReview>()
 
+    var listreview = arrayListOf<infoReview>()
+    var check : Boolean = false
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.selecthomeitemfragment, container, false)
         val storage = FirebaseStorage.getInstance()
         val database = FirebaseDatabase.getInstance()
         val idx = activity?.intent?.getStringExtra("cononame")
         val conoRef = database.getReference("Cono/${GlobalApplication.area1}/${GlobalApplication.area2}/${GlobalApplication.area3}/${idx}")
+        val userRef = database.getReference("User/${GlobalApplication.prefs.getString("userid","0")}")
         val mRecyclerView=view.findViewById<RecyclerView>(R.id.reviewRV)
-
         //코인노래방 정보 입력
         conoRef.addListenerForSingleValueEvent(object: ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
@@ -41,19 +43,13 @@ class selecthomeitemFragment : Fragment(){
                 for (snapshot in p0.children){
                     if(snapshot.key.equals("info")){
                         Glide.with(requireContext()).load(snapshot.child("image").value.toString()).apply(RequestOptions.bitmapTransform(MultiTransformation(CenterCrop()))).into(view.cono_image)
-                        /*
-                        val ref = storage.getReferenceFromUrl(snapshot.child("image").value.toString())
-                        ref.getBytes(Long.MAX_VALUE).addOnSuccessListener { bytes->
-                            val bmp= BitmapFactory.decodeByteArray(bytes,0,bytes.size)
-                            cono_image?.setImageBitmap(bmp)
-                        }.addOnFailureListener{}*/
                         cono_name.text = p0.key.toString()
                         cono_address.text = snapshot.child("address").value.toString()
                         cono_callnum.text = snapshot.child("tel").value.toString()
                         cono_payinfotxt.text = snapshot.child("charge/songs/500").value.toString()+" 500원, "+snapshot.child("charge/songs/1000").value.toString()+" 1000원"
                         cono_payinfotxt2.text = "30분 "+snapshot.child("charge/time/30분").value.toString()+"원, 1시간 "+snapshot.child("charge/time/1시간").value.toString()+"원"
                     }else if(snapshot.key.equals("Review")) {
-                        reviewtext.visibility = VISIBLE
+                        //reviewtext.visibility = VISIBLE
                         for(every in snapshot.children){
                             listreview.add(infoReview(every.child("name").value.toString(),every.child("review_content").value.toString(),every.child("rating").value.toString().toFloat()))
                         }
@@ -65,19 +61,38 @@ class selecthomeitemFragment : Fragment(){
             }
         })
 
-
+        /*userRef.child("bookmark").addListenerForSingleValueEvent(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {}
+            override fun onDataChange(p0: DataSnapshot) {
+                for(snapshot in p0.children){
+                    check = snapshot.key.equals("${idx}")
+                }
+            }
+        })*/
 
         // 즐겨찾기 표시
         view.bookmark.setOnClickListener {
             if(check==false){
+                conoRef.addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onCancelled(p0: DatabaseError) {}
+                    override fun onDataChange(p0: DataSnapshot) {
+                        for(snapshot in p0.children){
+                            if(snapshot.key.equals("info")){
+                                userRef.child("bookmark/${idx}").setValue(snapshot.value)
+                            }
+                        }
+                    }
+                })
                 check = true
                 bookmark.setImageResource(R.drawable.star2)
             }
             else{
                 check = false
                 bookmark.setImageResource(R.drawable.star)
+                userRef.child("bookmark/${idx}").removeValue()
             }
         }
+
         return view
     }
 
