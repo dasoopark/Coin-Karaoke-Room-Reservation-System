32463f3ceebbebd584c8b37c303bd439b22d62e3
dasoopark <ektn0514@gmail.com>
2020-08-10 04:58:53 +0900

    Update payment_songFragment.kt

--- app/src/main/java/kr/co/korearental/dongno/payment_songFragment.kt
+++ app/src/main/java/kr/co/korearental/dongno/payment_songFragment.kt
@@ -41,6 +41,7 @@ class payment_songFragment: Fragment() {
 
         view.time_choiceButton.setOnClickListener{
             val cal = Calendar.getInstance()
+<<<<<<< Updated upstream
 
             // Pass activity reference to Builder and set your OnDateTimeSetListener
             DateTimePicker.Builder(requireActivity())
@@ -52,13 +53,30 @@ class payment_songFragment: Fragment() {
                     cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
                     cal.set(Calendar.MINUTE, minute)
 
+=======
+
+            // Pass activity reference to Builder and set your OnDateTimeSetListener
+            DateTimePicker.Builder(requireActivity())
+                .onDateTimeSetListener { year, month, dayOfMonth, hourOfDay, minute ->
+                    // Use selected date and time values
+                    cal.set(Calendar.YEAR, year)
+                    cal.set(Calendar.MONTH, month)
+                    cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+                    cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    cal.set(Calendar.MINUTE, minute)
+
+>>>>>>> Stashed changes
                    // 변수 => year, month, dayofmonth, hoursOfDay, minute 으로 쓰면됨
                     reservation_time.text = SimpleDateFormat("YY년 MM월 dd일 HH시 mm분").format(cal.time)
                 }
                 .build()
                 .show()
         }
+<<<<<<< Updated upstream
         
+=======
+
+>>>>>>> Stashed changes
         return view
     }
 
