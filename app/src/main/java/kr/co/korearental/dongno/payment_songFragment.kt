04af77d855875aed02dbd9bc7a8b140af19cfa24
dasoopark <ektn0514@gmail.com>
2020-08-10 20:14:07 +0900

    ui

--- app/src/main/java/kr/co/korearental/dongno/payment_songFragment.kt
+++ app/src/main/java/kr/co/korearental/dongno/payment_songFragment.kt
@@ -1,32 +1,45 @@
 package kr.co.korearental.dongno
 
 
+
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.view.menu.MenuBuilder
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
-import com.mohamedabulgasem.datetimepicker.DateTimePicker
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.mohamedabulgasem.datetimepicker.DateTimePicker
 import kotlinx.android.synthetic.main.payment_song.*
 import kotlinx.android.synthetic.main.payment_song.view.*
+import me.kareluo.ui.OptionMenu
+import me.kareluo.ui.OptionMenuView.OnOptionMenuClickListener
+import me.kareluo.ui.PopupMenuView
+import me.kareluo.ui.PopupView
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class payment_songFragment: Fragment() {
 
-     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,  savedInstanceState: Bundle? ): View? {
-        val view = inflater.inflate(R.layout.payment_song, container, false)
+     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+     @SuppressLint("RestrictedApi")
+     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+         val view = inflater.inflate(R.layout.payment_song, container, false)
 
-        view.room_choiceButton.setOnClickListener{
+
+         //방 선택하기
+         view.room_choiceButton.setOnClickListener{
             val builder = AlertDialog.Builder(requireContext())
             val dialogView = layoutInflater.inflate(R.layout.roomchoice_dialog, null)
             builder.setView(dialogView)
@@ -39,10 +52,10 @@ class payment_songFragment: Fragment() {
             // Dialog 사이즈 조절 하기
         }
 
+
+        // 예약 시간 선택
         view.time_choiceButton.setOnClickListener{
             val cal = Calendar.getInstance()
-<<<<<<< Updated upstream
-
             // Pass activity reference to Builder and set your OnDateTimeSetListener
             DateTimePicker.Builder(requireActivity())
                 .onDateTimeSetListener { year, month, dayOfMonth, hourOfDay, minute ->
@@ -53,31 +66,15 @@ class payment_songFragment: Fragment() {
                     cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
                     cal.set(Calendar.MINUTE, minute)
 
-=======
-
-            // Pass activity reference to Builder and set your OnDateTimeSetListener
-            DateTimePicker.Builder(requireActivity())
-                .onDateTimeSetListener { year, month, dayOfMonth, hourOfDay, minute ->
-                    // Use selected date and time values
-                    cal.set(Calendar.YEAR, year)
-                    cal.set(Calendar.MONTH, month)
-                    cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
-                    cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
-                    cal.set(Calendar.MINUTE, minute)
-
->>>>>>> Stashed changes
                    // 변수 => year, month, dayofmonth, hoursOfDay, minute 으로 쓰면됨
                     reservation_time.text = SimpleDateFormat("YY년 MM월 dd일 HH시 mm분").format(cal.time)
                 }
                 .build()
                 .show()
         }
-<<<<<<< Updated upstream
-        
-=======
 
->>>>>>> Stashed changes
         return view
     }
 
 }
+
